aaf98aa63b8eae5d5f7454aabe7edfd5660d2f0c
conejinpeople <79195401+conejinpeople@users.noreply.github.com>
2021-03-08 21:23:16 +0100

    Update Puzzle1.py

--- Puzzle1.py
+++ Puzzle1.py
@@ -1,10 +1,9 @@
 import getpass #la libreria nos oculta la informacion introducida por teclado
 class Rfid:
-   
+
     def read_uid(self):
         # cogemos la entrada de teclado (la tarjeta funciona como uno)
-        print("Pasa la tarjeta")
-        numB = int(getpass.getpass(prompt="Pasa la tarjeta:" ))
+        numB = int(getpass.getpass(prompt="")) #Colocamos un espacio de prompt sino nos imprimer "password"
         # traducimos el int a hexadecimal y lo pasamos a mayusculas
         self.num = hex(numB).upper()[2:] #con el 2: cogemos el valor introducido a partir del segundo caracter saltandonos el 0x
         return self.num
@@ -13,4 +12,5 @@ if __name__ == "__main__":
     # llamamos a la clase, inicializamos la definicion y imprimimos
     rf = Rfid()
     uid = rf.read_uid()
+    print ("\033[A\033[A") #limpiamos la linea previa y colocamos el cursor en ella
     print(uid)
